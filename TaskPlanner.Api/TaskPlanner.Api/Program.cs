20d222bde92cb583cf8e44a10b2585ef67e0d4d7
shreelakshmi23 <91884535+Shree9741@users.noreply.github.com>
2026-05-21 17:32:50 +0530

    Enable Swagger in all environments

--- TaskPlanner.Api/TaskPlanner.Api/Program.cs
+++ TaskPlanner.Api/TaskPlanner.Api/Program.cs
@@ -24,11 +24,10 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+
 
 app.UseHttpsRedirection();
 app.UseCors("AllowReactApp");
